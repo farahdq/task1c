659a2f8cd2c7d3b4fe785f680caf63a7d6ef2a77
farahdq <73812417+farahdq@users.noreply.github.com>
2021-12-05 14:55:47 +0700

    Delete assignment 2 directory

--- assignment 2/Javascripts/Geometries/Cylinder.js
+++
@@ -1,9 +0,0 @@
-import {RegularPolygonPrism} from "./RegularPolygonPrism.js";
-
-export class Cylinder extends RegularPolygonPrism {
-    static NUMBER_OF_VERTICE = 240;
-
-    constructor(position, height, color, lowerRadius, upperRadius) {
-        super(position, height, Cylinder.NUMBER_OF_VERTICE, color, lowerRadius, upperRadius);
-    }
-}
